033ca3f2d2732a854bddd99a72acb9b619ca9260
Mhonns <nsamaira@cmkl.ac.th>
2024-08-23 05:33:02 +0700

    merging frameee17 and main branches

---
+++ src/pages/wolfare_backend.py
@@ -0,0 +1,31 @@
+# wolfare-backend.py
+# 
+# This program provides useful functions to communicate between ui and
+# the API server.
+#                       Created by Nathadon Samairat 23 August 2024
+
+import requests
+
+def sanitize():
+    pass
+
+# returns - sanitized news, - date modified
+def formatReceivedNews():
+    pass
+
+def formatUserPrompt(prompt):
+    pass
+
+def formatUploadData():
+    pass
+
+def sendRequest(message):
+    try:
+        # Send a POST request to the server
+        response = requests.post(server_url, json=data)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            pass
+    except:
+        print("Error while sending the request")
