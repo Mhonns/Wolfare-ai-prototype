b30d50fb0590814bc640dbbe199065bada4bab50
Mhonns <nsamaira@cmkl.ac.th>
2024-08-23 21:00:35 +0700

    Add more functions and details to the backend

--- src/client_scripts/wolfare_backend.py
+++ src/client_scripts/wolfare_backend.py
@@ -5,27 +5,75 @@
 #                       Created by Nathadon Samairat 23 August 2024
 
 import requests
+server_ip = "x.x.x.x"
+server_port = "xxxx"
+server_url = server_ip + ":" + server_port
 
-def sanitize():
+history_cache = {'History 1' : "", 'History 2' : "", 'History 3' : ""}
+fetching = False
+prompting = False
+pushing = False
+
+def saveCache(id, cache):
+    print("Chat cache save at: ", id)
+    history_cache[id] = cache
+
+def sendRequest(method, message):
+    try:
+        if method == "News":
+            response = requests.get(server_url + "/api/news")
+            if response.status_code == 200:
+                return response
+            else:
+                return "Error while getting the output" + str(response.status_code)
+        elif method == "Prompt":
+            response = requests.post(server_url + "/api/prompt", json=message)
+            if response.status_code == 200:
+                return response
+            else:
+                return "Error while getting the output" + str(response.status_code)
+    except:
+        return "Error while sending the request"
+
+def sanitizer():
+    pass
+
+def newsFormater(data):
+    # TODO please format the news data
+    pass
+
+def dataInputFormater(data):
+    # TODO please format the data
     pass
 
 # returns - sanitized news, - date modified
-def formatReceivedNews():
-    pass
+def fetchNews():
+    global fetching
+    if fetching == False:
+        fetching = True
+        date = "dd-mm-yyyy"
+        news = "Loading the output"
+        # data = sendRequest("NEWS", None)
+        # date, news = newsFormater(data)
+        fetching = False
+        return date, news
+    else:
+        return "Waiting for the previous news to be fetched. please try again"
 
-def formatUserPrompt(prompt):
-    pass
+def sendPrompt(prompt):
+    global prompting
+    if prompting == False:
+        # output = sendRequest("PROMPT", prompt)
+        output = "TODO send the promt request"
+        return output
+    else:
+        return "Waiting for the previous prompt to be fetched. please try again"
 
-def formatUploadData():
-    pass
-
-def sendRequest(message):
-    try:
-        # Send a POST request to the server
-        response = requests.post(server_url, json=data)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            pass
-    except:
-        print("Error while sending the request")
+def pushToCloud(data):
+    global pushing
+    if pushing == False:
+        # output = sendRequest("PUSH", data)
+        output = "TODO send the push request"
+        return output
+    else:
+        return "Waiting for the previous data to train our model and verifying. please try again"
