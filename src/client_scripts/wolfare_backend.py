cf33a653ead0d1bbfa19e2041da119958f0ce084
Mhonns <nsamaira@cmkl.ac.th>
2024-08-25 01:31:20 +0700

    Send the request to the api server for all functions

--- src/client_scripts/wolfare_backend.py
+++ src/client_scripts/wolfare_backend.py
@@ -6,11 +6,11 @@
 
 import requests
 import json
-import os
 
-server_ip = "x.x.x.x"
-server_port = "xxxx"
-server_url = server_ip + ":" + server_port
+protocal = "http://"
+server_ip = "127.0.0.1"
+server_port = "2546"
+server_url = protocal + server_ip + ":" + server_port
 
 history_cache = {'History 1' : "", 'History 2' : "", 'History 3' : ""}
 fetching = False
@@ -21,22 +21,40 @@ def saveCache(id, cache):
     print("Chat cache save at: ", id)
     history_cache[id] = cache
 
-def sendRequest(method, message):
+def sendRequest(method, message=None):
     try:
         if method == "News":
             response = requests.get(server_url + "/api/news")
             if response.status_code == 200:
-                return response
+                data = response.json()
+                date = data.get("date")
+                output = data.get("output")
+                return date, output
+            else:
+                return "Error", f"Error while getting the output: {response.status_code}"
+        elif method == "Prompt":
+            response = requests.post(server_url + "/api/prompt", json={"content": message})
+            if response.status_code == 200:
+                data = response.json()
+                output = str(data.get("output"))
+                return output
             else:
                 return "Error while getting the output" + str(response.status_code)
-        elif method == "Prompt":
-            response = requests.post(server_url + "/api/prompt", json=message)
+        elif method == "Push":
+            # response = requests.post(server_url + "/api/add_data", json={"content": message})
+            # if response.status_code == 200:
+            #     return response
+            # else:
+            #     return "Error while getting the output" + str(response.status_code)
+            return "This is our future plan"
+        elif method == "PushJSON":
+            response = requests.post(server_url + "/api/add_json_data", json=message)
             if response.status_code == 200:
                 return response
             else:
                 return "Error while getting the output" + str(response.status_code)
     except:
-        return "Error while sending the request"
+        return "Error while sending the request", None
 
 def newsFormater(data):
     # TODO please format the news data
@@ -51,20 +69,18 @@ def fetchNews():
     global fetching
     if fetching == False:
         fetching = True
-        date = "dd-mm-yyyy"
-        news = "Loading the output"
-        # data = sendRequest("NEWS", None)
-        # date, news = newsFormater(data)
+        date, news = sendRequest("News")
         fetching = False
         return date, news
     else:
-        return "Waiting for the previous news to be fetched. please try again"
+        return "Fail to retrieve latest date", "Waiting for the previous news to be fetched. please try again"
 
 def sendPrompt(prompt):
     global prompting
     if prompting == False:
-        # output = sendRequest("PROMPT", prompt)
-        output = "TODO send the promt request"
+        prompting = True
+        output = sendRequest("Prompt", prompt)
+        prompting = False
         return output
     else:
         return "Waiting for the previous prompt to be fetched. please try again"
@@ -72,8 +88,11 @@ def sendPrompt(prompt):
 def pushToCloud(data):
     global pushing
     if pushing == False:
+        pushing = True
+        print(data)
         # output = sendRequest("PUSH", data)
         output = "TODO send the push request"
+        pushing = False
         return output
     else:
         return "Waiting for the previous data to train our model and verifying. please try again"
@@ -86,7 +105,9 @@ def uploadFile(path_file):
         with open(path_file, 'r', encoding='utf-8') as file:
             data = file.read()
         json_data = json.loads(data)
-        return 0, json_data
+        for i in range(100):
+            sendRequest("PushJSON", json_data[i])
+        return 0, None
     except json.JSONDecodeError as e:
         return 3, str(e)
     except Exception as e:
